a147798ee8efc1cf62c25592fac2110f2a98fd53
Valerio <valiokei@gmail.com>
2020-10-09 15:42:43 +0200

    Bycicle kinematic Odometry class

---
+++ BicycleKinematicOdometry/BicycleKinematicOdometry.py
@@ -0,0 +1,234 @@
+import math
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+
+class BicycleKinematicOdometry:
+
+    # Memory variables
+    # this array save the previous-step encoder position, not the increments!
+    oldRotationAngle = [0, 0, 0, 0]
+    poseArray = np.array([0, 0, 0])
+
+    def __init__(self):
+        self.wheelRadius = 0
+        self.delta_T = 0
+        self.Lr = 0
+        self.Lf = 0
+
+# wheelRadius,delta_T,Lr,Lf
+
+    def SetWheelRadius(self, wheelRadius):
+        self.wheelRadius = wheelRadius
+
+    def SetDelta_t(self, delta_T):
+        self.delta_T = delta_T
+
+    def SetLr(self, Lr):
+        self.Lr = Lr
+
+    def SetLf(self, Lf):
+        self.Lf = Lf
+
+    def SetStaticData(self, wheelRadius, delta_T, Lr, Lf):
+        self.SetWheelRadius(wheelRadius)
+        self.SetDelta_t(delta_T)
+        self.SetLr(Lr)
+        self.SetLf(Lf)
+
+    def ComputeVelocity(self, wheelsRotationAngle, wheelIndex,):
+        '''
+        This function allows to compute the wheel linear velocity.
+        Parmameters
+        ----------
+        Return
+        ----------
+        '''
+        # sign compute: note thath it is inverted because of UE4 reverse implementation
+        # NOTE: in UE4 negative encoder value rapresents a forward motion and viceversa.
+        rotationSign = 1
+        if (wheelsRotationAngle <= 0):
+            rotationSign = 1
+        else:
+            rotationSign = -1
+
+        # this rapresent the threeshold after whichi consider encoder saturation. note: this refers to the old encoder value
+        hightSaturationThreshoold = 1200
+        # this rapresent the threeshold below whichi consider encoder saturation. Note: this refers to the new encoder value
+        lowSaturationThreshoold = 200
+        # this value save how much the wheel is rotate from the previous step to the actual
+        rotationAngleDifference = 0
+
+        # convert into positiv logic: forward is now negative for UE4
+        oldRA = self.oldRotationAngle[wheelIndex] * (-1)
+        newRA = wheelsRotationAngle * (-1)
+
+        # TODO: I think the following if can better coding with switch case and other structure
+        # and several cases can be compress,but for quickest learning i prefer to leave everything as it is.
+        # For testing tasks i don't think this should be a computational problem!
+
+        # Forward
+        if(oldRA > 0 and newRA > 0):
+            # forward without encoder saturation
+            if(newRA > oldRA):
+                rotationAngleDifference = newRA - oldRA
+            # forward with encoder saturation
+            if(oldRA > newRA and (oldRA > hightSaturationThreshoold and newRA < lowSaturationThreshoold)):
+                rotationAngleDifference = 1800 - (oldRA - newRA)
+
+        # Forward from negative value to positives
+        if(oldRA < 0 and newRA > 0):
+            rotationSign = 1
+            rotationAngleDifference = newRA-oldRA
+
+        # Forward from negative value to smaller negative ones
+        if(oldRA < 0 and newRA < 0):  # both negative
+            # be sure that i'm not going to saturating the encoder
+            if(oldRA > -hightSaturationThreshoold):
+                if(oldRA < newRA):  # moving to zero value
+                    rotationSign = 1
+                    rotationAngleDifference = newRA - oldRA
+
+        # Backward
+        if(oldRA < 0 and newRA < 0):
+            # backward without encoder saturation
+            if(newRA < oldRA):
+                rotationAngleDifference = oldRA - newRA
+            # backward without encoder saturation
+            if(oldRA < -hightSaturationThreshoold and newRA > -lowSaturationThreshoold):
+                rotationAngleDifference = 1800 + (oldRA - newRA)
+
+        # Backward from positive value and remaining in positive value
+        if(oldRA > 0 and newRA > 0):  # positive values
+            # be sure that i'm not going to saturating the encoder
+            if(oldRA < hightSaturationThreshoold):
+                if(oldRA > newRA):  # backward motion
+                    rotationSign = -1
+                    rotationAngleDifference = oldRA-newRA
+
+        # Backward from positive value to negative ones
+        if(oldRA > 0 and newRA < 0):
+            rotationSign = -1
+            rotationAngleDifference = oldRA - newRA
+
+        # the wheel circunference
+        wheelCircumference = 2*math.pi*self.wheelRadius
+        # the velocity of the wheel: space/delta_T
+        v = (wheelCircumference * (abs(rotationAngleDifference)/360))/self.delta_T
+
+        # update the memory variable with new old value
+        self.oldRotationAngle[wheelIndex] = wheelsRotationAngle
+
+        return [v, rotationSign]
+
+    def OdometryCar(self, wheelsSteerAngle, wheelsRotationAngle):
+        # this Odometry model is based on the equations shows in https://www.youtube.com/watch?v=HqNdBiej23I
+
+        # NOTE: this is an assumption!!!
+        # We approssimate the steerangle of the car by mean doing
+        # the mean of the front-steering-wheels, that are, for our car, the only that changing their direction.
+        # according to  http://www.ce.unipr.it/~medici/ctrl.pdf for relative small angle the approssimation is fine
+        wheelsSteerAngle = np.mean([wheelsSteerAngle[0], wheelsSteerAngle[1]])
+
+        # vehicle velocity
+        # compute the velocity for each wheel
+        wheelsRotationAngle = np.asarray(wheelsRotationAngle)[0]
+        v0 = self.ComputeVelocity(wheelsRotationAngle[0], 0)
+        v1 = self.ComputeVelocity(wheelsRotationAngle[1], 1)
+        v2 = self.ComputeVelocity(wheelsRotationAngle[2], 2)
+        v3 = self.ComputeVelocity(wheelsRotationAngle[3], 3)
+
+        allV = np.array([v0[0], v1[0], v2[0], v3[0]])
+
+        # NOTE: APPORSSIMATION!!
+        # we decide to take the mean of the 4 wheel, but it is possibile also to use
+        # the slowest or faster 2 wheel, or the fronts ones, or the back ones.
+        # It is your choise
+
+        # CODE to use the 2 fasters
+        # max1 = np.sort(allV)[len(allV)-1]
+        # max2 = np.sort(allV)[len(allV)-2]
+        # v = np.mean([max1, max2])
+
+        # Code to use the mean
+        v = np.mean(allV)
+
+        # can happen that some wheel became negative before the others
+        # and the car is not going backward ( positive logic motion ( see inside ComputeVelocity))
+        # so we decide thath the direction of movement of the car change only when all the wheel are rotating
+        # in the same towards
+        rotationSign = v0[1]
+        if (v0[1] == v1[1] == v2[1] == v3[1]):
+            rotationSign = v0[1]
+
+        # Compute vehicle orientation angle
+        # change steering sign to be compliance with UE4 gps data
+        if(rotationSign == 1):
+            yawAngle = wheelsSteerAngle * (-1) # this value is in degree
+        else:
+            yawAngle = wheelsSteerAngle   # this value is in degree
+
+        yawAngle = yawAngle * (math.pi/180)  # convert degree to radians
+
+        # vehicle sideslip Angle  (beta)
+        tan = math.tan(yawAngle)
+        beta = math.atan((self.Lr/(self.Lr+self.Lr))
+                         * tan)  # beta is in radians
+
+        # Compute of the velocity kinematic model
+
+        # first compute the car orientation
+        yawRate = (v/(self.Lr*2)) * math.cos(beta) * math.tan(yawAngle)
+        newYawAngle = yawRate * self.delta_T
+
+        # second compute the components of the velocity vector.
+        # NOTE: with bycicle model we ar assuming that this vector is positioned
+        # at distance Lr from the rear wheel and Lf from the front wheel
+        vx = v * math.cos(newYawAngle + beta)
+        vy = v * math.sin(newYawAngle + beta)
+
+        # From velocity model to odometric model
+        dx = vx * self.delta_T * rotationSign
+        dy = vy * self.delta_T * rotationSign
+
+        return [dx, dy, newYawAngle]
+
+    def carTrajector(self, wheelsSteerAngle, wheelsRotationAngle):
+        '''
+        This method compute the absolute pose from new data. 
+        The fixed Reference system used here is the one that the car have before its first shift
+        '''
+        # compute the odometry data
+        newPoseArray = self.OdometryCar(wheelsSteerAngle, wheelsRotationAngle)
+
+        # save data from previous step
+        oldPose = np.array(
+            [self.poseArray[0], self.poseArray[1], 0]).reshape([1, 3]).T
+        old_yaw = self.poseArray[2]
+
+        # We need to compute Roto-Translation from previous pose to the new one
+
+        # Compute Rotation Matrix
+        Rotation = R.from_euler('z', old_yaw, degrees=False)
+        Rotation_Matrix = Rotation.as_matrix()
+
+        # Compute Translation Matrix by attaching it to Rotation Matrix
+        Translation = np.concatenate([Rotation_Matrix, oldPose], axis=1)
+        T_r = np.concatenate([Translation, np.array(
+            [0, 0, 0, 1]).reshape([1, 4])], axis=0)
+
+        # Prepar odometry new data
+        update_pose = np.array(
+            [newPoseArray[0], newPoseArray[1], 0, 1]).reshape(1, 4).T
+
+        # Compute the dot product between Roto-Translation Matrix and new Odometry data
+        newPose = np.dot(T_r, update_pose).T[:, :-1]
+
+        # Prepare the output
+        output = np.array([newPose[0][0], newPose[0]
+                           [1], old_yaw+newPoseArray[2]])
+
+        # update the memory variable with new pose just calculate
+        self.poseArray = output
+
+        return output
---
+++ BicycleKinematicOdometry/tesina_macchina.py
@@ -0,0 +1,117 @@
+from unreal_api.environment import Environment
+#from VehicleOdometryInfo import OdometryCar
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+from matplotlib.path import Path
+import matplotlib.patches as patches
+from matplotlib import style
+from BicycleKinematicOdometry import BicycleKinematicOdometry
+
+
+sensor_settings = {
+    "RGBCamera": {
+        "width": 84,
+        "height": 84,
+        "channels": "RGB",
+        "FOV"	: 90,
+        "show": True
+    },
+    "GPS": {},
+    "CarEncoder": {}
+}
+
+action_manager_settings = {
+    "CarActionManager": {
+        "command_dict": {
+            "TURN": 0,
+            "STRAIGHT": 1
+        },
+        "settings": {
+        }
+    }
+}
+
+reset_manager_settings = {
+    "ContinuousResetManager": {}
+}
+
+observations_step = ["RGBCamera", "GPS", "CarEncoder"]
+
+
+env = Environment(port=9734, address='localhost', sensor_settings=sensor_settings,
+                  action_manager_settings=action_manager_settings, reset_manager_settings=reset_manager_settings, render=True)
+
+
+style.use('fivethirtyeight')
+
+# ********* PER IL NOSTRO *************
+figx = plt.figure()
+ax1 = figx.add_subplot(2,2,1)
+ax1.axis('equal')
+ax = figx.add_subplot(2,2,2)
+ax.axis('equal')
+xs = []
+ys = []
+#  ********* PER IL NOSTRO *************
+
+gxs = []
+gys = []
+
+def animateg(i):
+    observations = env.get_obs(observations_step)
+    gxs.append(observations[1][6])
+    gys.append(observations[1][7])
+
+
+    ax.clear()
+    ax.plot(gxs, gys,'b')
+
+
+#  ********* PER IL NOSTRO *************
+
+classObject = BicycleKinematicOdometry()
+def animatex(i):
+    
+    observations = env.get_obs(observations_step)
+    steerings = [observations[2][0], observations[2][2],
+                 observations[2][4], observations[2][6]]
+    rotations = [observations[2][1], observations[2][3],
+                 observations[2][5], observations[2][7]],
+
+    wheelRadius = observations[2][8]
+    delta_T = 0.1
+    # that mean that the center of mass of the car is in the middle between front and rear wheel
+    Lr = observations[2][10]/2 
+    Lf = observations[2][10]/2
+
+    classObject.SetStaticData(wheelRadius,delta_T,Lr,Lf)
+    trajectory = classObject.carTrajector(steerings,rotations)
+
+    # trajectory = KinematicBicycleModelOdometry.carTrajector(
+    #     steerings, rotations, observations[2][8], 0.1, observations[2][10]/2, observations[2][10]/2)
+
+
+    print('difference x: '+ str(trajectory[0]-observations[1][6]))
+    print('difference y: '+str(trajectory[1]-observations[1][7]))
+
+
+
+    print('difference yaw: ')
+    print(trajectory[2]-observations[1][10])
+
+    xs.append(trajectory[0])
+    ys.append(trajectory[1])
+
+    ax1.clear()
+    ax1.plot(xs, ys,'r')
+#  ********* PER IL NOSTRO *************
+
+
+ani = animation.FuncAnimation(figx, animateg, interval=100)
+# plt.show()
+
+
+#  ********* PER IL NOSTRO *************
+ani2 = animation.FuncAnimation(figx, animatex, interval=100)
+plt.show()
